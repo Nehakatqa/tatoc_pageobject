a8307a6273bc751671e81226d45ed68344429fd8
Nehakatqa <40543382+Nehakatqa@users.noreply.github.com>
2018-06-30 02:11:24 -0700

    Update AppTest.java

--- src/test/java/com/qait/project_name/Pom/AppTest.java
+++ src/test/java/com/qait/project_name/Pom/AppTest.java
@@ -26,10 +26,7 @@ public class AppTest
 		{
 			driver=new ChromeDriver();
 			lu=new launch_url(driver);
-			b=new basic_grid(driver);
-			f=new Frame_Dungeon(driver);
-			d=new drag_around(driver);
-			c=new cookie_handling(driver);
+			
 		}	
 			
 		
@@ -38,40 +35,45 @@ public class AppTest
 		{
 			lu.url();
 		}
-		@Test
+		@Test(dependsOnMethods={"Step01web_browser_launches"})
 		public void Step02click_on_basiccourse()
 		{
 			lu.click_on_basiccourse();
 		}
-		@Test
+		@Test(dependsOnMethods={"Step02click_on_basiccourse"})
 		public void Step04clickredbox()
 		{
 		b.click_on_redbox();
 		}
 		
-		@Test
+		@Test(dependsOnMethods={"Step04clickredbox"})
 		public void Step03clickgreenbox()
 		{
 			b.click_on_greenbox();
 		}
 		
-		@Test
+		@Test(dependsOnMethods={"Step03clickgreenbox"})
 		public void Step05frame2()
 		{
+			
+			f=new Frame_Dungeon(driver);
 			f.proceed_dragaround();
+			
 		}
-		@Test
+		@Test(dependsOnMethods={"Step05frame2"})
 		public void Step06dragaround()
 		{
+			d=new drag_around(driver);
 			d.proceed();
 		}
 		
-		@Test
+		@Test(dependsOnMethods={"Step06dragaround"})
 		public void Step07launchpopup()
 		{
+			c=new cookie_handling(driver);
 			pw.proceed();
 		}
-	
+		@Test(dependsOnMethods={"Step07launchpopup"})
 		public void Step0cookie_end()
 		{
 			c.proceed_end();
